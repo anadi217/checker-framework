cb1f46c1332c76ff27b30f9b65d0b70cf383faf0
Martin Kellogg <kelloggm@cs.washington.edu>
2017-03-13 10:54:14 -0700

    test for minlen2

---
+++ checker/tests/index/MinLenOfNewArray.java
@@ -0,0 +1,13 @@
+import org.checkerframework.checker.index.qual.*;
+
+
+class MinLenOfNewArray {
+    void test(int[] a) {
+        int[] b = new int[a.length + 1];
+        int k = b[0];
+    }
+
+    void test2(@Positive int a) {
+	int @MinLen(1) [] b = new int[a];
+    }
+}
