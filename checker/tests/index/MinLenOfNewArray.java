704313eedd2cb577f96bc1fb414537e5fca346ff
Martin Kellogg <kelloggm@cs.washington.edu>
2017-03-13 10:54:25 -0700

    formatting

--- checker/tests/index/MinLenOfNewArray.java
+++ checker/tests/index/MinLenOfNewArray.java
@@ -1,6 +1,5 @@
 import org.checkerframework.checker.index.qual.*;
 
-
 class MinLenOfNewArray {
     void test(int[] a) {
         int[] b = new int[a.length + 1];
@@ -8,6 +7,6 @@ class MinLenOfNewArray {
     }
 
     void test2(@Positive int a) {
-	int @MinLen(1) [] b = new int[a];
+        int @MinLen(1) [] b = new int[a];
     }
 }
